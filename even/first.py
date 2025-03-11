002cfd1a3850d9e4f9448ddd2a0583a921bfef04
tejupriya123 <tejapriyaks9900@gmail.com>
2025-03-11 14:18:12 +0530

    Create first.py

---
+++ even/first.py
@@ -0,0 +1,13 @@
+n1=float(input("enter first number:"))
+n2=float(input("enter second number:"))
+operation = (input("enter operation(+,-,*,/):"))
+if operation=="+":
+   print(n1+n2)
+elif operation=="-":
+        print(n1-n2)
+elif operation=="*"
+        print(n1*n2)
+elif operation=="/"
+        print(n1/n2)
+ else:
+        print("invalid operator")
