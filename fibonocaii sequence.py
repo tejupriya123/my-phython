4340aeef62514c39afff9ed0e880b39a2c6c2e67
tejupriya123 <tejapriyaks9900@gmail.com>
2025-03-12 12:35:23 +0530

    Create fibonocaii sequence.py

---
+++ fibonocaii sequence.py
@@ -0,0 +1,10 @@
+from typing import Sequence
+def fibonacci(n)
+sequence = []
+a,b =0,1
+for i in range(n):
+  sequence,append(a)
+  a,b=b,a+b
+  return Sequence
+   terms=int(input("enter the number of terms"))
+   print("fibonacci sequence",terms)
