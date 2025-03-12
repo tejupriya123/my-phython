ef39d27c33fa796951dd36498fba161520cb1f16
tejupriya123 <tejapriyaks9900@gmail.com>
2025-03-12 14:26:35 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,9 @@
+def count_vowels(input_string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    
+    for char in input_string:
+        if char in vowels:
+            count += 1
+    print("teju-070")
+    return count
