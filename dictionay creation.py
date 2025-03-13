9ae18b8192724fb7127d3fa866f25ea4002b4b4e
tejupriya123 <tejapriyaks9900@gmail.com>
2025-03-13 15:26:22 +0530

    Create dictionay creation.py

---
+++ dictionay creation.py
@@ -0,0 +1,14 @@
+def create_and_print_dictionary():
+    print("teju-070")
+    my_dict = {
+        "name": "Alice",
+        "age": 25,
+        "city": "New York"
+    }
+    
+    
+    print("Created Dictionary:", my_dict)
+
+
+create_and_print_dictionary()
+
