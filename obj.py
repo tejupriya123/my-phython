4404a70829071321f2d5cf9064d920875a25b485
tejupriya123 <tejapriyaks9900@gmail.com>
2025-03-14 10:52:51 +0530

    Create obj.py

---
+++ obj.py
@@ -0,0 +1,9 @@
+class chinni:
+  def __init__(self):
+    self.ammi="hi dummi!"
+    self.__dubbi="you are sick!"
+  def teju(self):
+    print(self.__dubbi)
+obj=chinni()
+print(obj.ammi)
+obj.teju()
